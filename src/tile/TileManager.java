473d2c14fade8740e6ec8ec480d838cd68504ce5
LGB <bao2002pytn@gmail.com>
2022-11-21 15:49:11 +0700

    Update2

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,5 +1,6 @@
 package tile;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,7 +15,7 @@ public class TileManager {
     GamePanel gp;
     public Tile[] tile;
     public int mapTileNum[][];
-    
+    boolean drawPath = true;
     public TileManager(GamePanel gp) {
         
         this.gp = gp;
@@ -157,5 +158,16 @@ public class TileManager {
                
             }
         }
+        // if(drawPath == true){
+        //     g2.setColor(new Color(255,0,0,70));
+        //     for(int i=0;i<gp.pFinder.pathList.size();i++){
+        //         int worldX = gp.pFinder.pathList.get(i).col * gp.tileSize;
+        //         int worldY = gp.pFinder.pathList.get(i).row * gp.tileSize;
+        //         int screenX =worldX -gp.player.worldX + gp.player.screenX;
+        //         int screenY =worldY -gp.player.worldY + gp.player.screenY;
+                
+        //         g2.fillRect(screenX, screenY, gp.tileSize, gp.tileSize);
+        //     }
+        // }
     }
 }
